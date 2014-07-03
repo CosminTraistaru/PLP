985930496820584e3b23d0220c9845bb17d5407c
Cosmin Traistaru <cosmin@traista.ru>
2014-07-03 11:37:49 +0300

    pep8 79c

--- p5.py
+++ p5.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 #     Problem 5:
-# Please write a program to generate all sentences where subject is in ["I", "You"] and verb is in ["Play", "Love"] and the object is in ["Hockey","Football"].
+# Please write a program to generate all sentences where subject is in 
+# ["I", "You"] and verb is in ["Play", "Love"] and the object is in 
+# ["Hockey","Football"].
 #
 # Hints:
 # Use list[index] notation to get a element from a list
