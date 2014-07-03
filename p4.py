24f4a2349ecdaca75f98374ac3aca537b39fe708
Cosmin Traistaru <cosmin@traista.ru>
2014-07-03 17:57:13 +0300

    add map to solution

--- p4.py
+++ p4.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 #     Problem 4:
-# Write a program that accepts a sentence and calculate the number of letters and digits.
+# Write a program that accepts a sentence and calculate the number of 
+# letters and digits.
 # Suppose the following input is supplied to the program:
 # hello world! 123
 # Then, the output should be:
@@ -9,24 +10,23 @@
 # DIGITS 3
 
 
-def getString():
+from collections import Counter
+
+
+def get_string():
     text = raw_input("Enter input: ")
     return text
 
 
 def count(text):
-    letters = 0
-    digits = 0
-    for character in text:
-        if character.isalpha():
-            letters += 1
-        if character.isdigit():
-            digits += 1
-    return [letters, digits]
+    print "Digits {}".format(Counter(map(str.isdigit, text))[1])
+    print "Letters {}".format( Counter(map(str.isalpha, text))[1])
+ #    return [letters, digits]
 
 
-text = getString()
+
+text = get_string()
 counted = count(text)
-print "Letters {}".format(counted[0])
-print "Digits {}".format(counted[1])
+# print "Letters {}".format(counted[0])
+# print "Digits {}".format(counted[1])
 
