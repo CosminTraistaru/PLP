7a51331d2f7577dbdbd1c979d3c88f52412390da
Cosmin Traistaru <cosmin@traista.ru>
2015-04-09 18:30:42 +0300

    lower than a number

--- PLP/palindrom.py
+++ PLP/palindrom.py
@@ -1,19 +1,21 @@
 
-
 def check_palindrome(num):
     list_of_num = []
+    original_number = num
     while num % 10 > 0:
         list_of_num.append(num % 10)
         num /= 10
     number_of_elements = len(list_of_num)
     for i in xrange(0, len(list_of_num)/2):
         if not list_of_num[i] == list_of_num[number_of_elements - i - 1]:
-            return False
+            print "{} not a pal".format(original_number)
+        else:
+            print "{} is palindrome".format(original_number)
 
 
 if __name__ == "__main__":
     number = 123456787654321
-    if check_palindrome(number) is not False:
-        print "{number} is a palindrome".format(number=number)
-    else:
-        print "not"
+    check_palindrome(number)
+
+    numbers = range(1, 1000)
+    map(check_palindrome, numbers)  # all palindrome lower than 1000
