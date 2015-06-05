27456eca912c5fba522cfe14347f9df18fc11357
Cosmin Traistaru <cosmin@traista.ru>
2015-06-05 15:21:00 +0300

    decorator

---
+++ PLP/decorator.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def timer(func):
+    def wrapper():
+        t1 = datetime.now()
+        func()
+        timer = datetime.now() - t1
+        print timer
+    return wrapper
+
+
+@timer
+def this_takes_long_time():
+    a = []
+    for i in range(1003300):
+        a.append({'a': {'b': []}})
+
+this_takes_long_time()
