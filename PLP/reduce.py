d5ca7f478271643c5917f925f786b2308853d649
Cosmin Traistaru <cosmin@traista.ru>
2015-09-28 21:07:14 +0300

    removed files

--- PLP/reduce.py
+++
@@ -1,9 +0,0 @@
-def my_reduce(func, iterable, initializer=None):
-    result = initializer
-    for it in iterable:
-        result = func(result, it)
-    return result
-
-
-print reduce(lambda x, y: x*y, [1, 2, 3, 4, 5], 1)
-print my_reduce(lambda x, y: x*y, [1, 2, 3, 4, 5], 1)
--- PLP/upper.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-if __name__ == '__main__':
-    print "Give me your name and money!"
-    read = os.read(0, 20)
-    print "".join(reversed(read)).upper()
