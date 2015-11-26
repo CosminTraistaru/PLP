669c2897e48e21d0aeeacc354a26c876531aceb8
Cosmin Traistaru <cosmin@traista.ru>
2015-11-26 18:09:28 +0200

    simplified prime function

--- PLP/functions_1_prime.py
+++ PLP/functions_1_prime.py
@@ -2,15 +2,15 @@ import os
 
 
 def prime(num):
-    if num < 2:
-        return False
-    if num in (2, 3):
-        return True
-    for n in xrange(2, num-1):
-        if num % n == 0:
-            return False
-    return True
-
+    # if num < 2:
+    #     return False
+    # if num in (2, 3):
+    #     return True
+    # for n in xrange(2, num-1):
+    #     if num % n == 0:
+    #         return False
+    # # return True
+    return all(num % n != 0 for n in xrange(2, num-1))
 
 if __name__ == '__main__':
     print "Give me your name and money!"
